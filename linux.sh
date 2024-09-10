c48bd64ddf125c4b8ceedacaff46e9ec9ad19419
Nedutechboy <chinedu2321@gmail.com>
2024-09-10 19:10:10 +0100

    working on a game

---
+++ linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " Welcome to my world of Devops"
+
+echo " ##################3########"
+
+
+echo " let play a guess game"
+
+read -p "pick a number from 1-10" $NUM
+
+if $NUM is gt-
