ca31de935f2f75b2d29cf80df79bdc5b9d2ff710
Nedutechboy <chinedu2321@gmail.com>
2024-09-18 23:55:07 +0100

    ok

--- linux.sh
+++ linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#######################
+
+# Author: Chinedu.Echenim
 
 echo " Welcome to my world of Devops"
 
@@ -7,6 +10,15 @@ echo " ##################3########"
 
 echo " let play a guess game"
 
-read -p "pick a number from 1-10" $NUM
+read " enter a number"$NUM
 
-if $NUM is gt-
+Mychoice=1
+
+for 1 in $(seq 1$Mychoice):
+	No=No_$RANDOM
+	if $NUM is -gt $No;then
+	   echo " you win"
+	else
+	    echo " you lose"
+
+done	    
